7630def99af5dce3d9ef11f950dc444f1cfdedd6
daicabrera <iodenteken@hotmail.com>
2023-10-20 15:27:13 -0300

    create entity in Domain

---
+++ src/Empresa.Negocio.Domain/Booking/BookingEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Business.Domain.Booking
+{
+    public class BookingEntity
+    { public int BookingId { get; set; }
+        public DateTime RegistrerDate { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public int CustomerId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ src/Empresa.Negocio.Domain/Customer/CustomerEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Business.Domain.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
