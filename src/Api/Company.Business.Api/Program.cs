7a37aa2bc4819fdb3eac3c6d6e9be0db11661b7f
IodenKisuka <iodenteken@hotmail.com>
2023-12-19 09:43:54 -0300

    config AddScope IdatabaseService

--- src/Api/Company.Business.Api/Program.cs
+++ src/Api/Company.Business.Api/Program.cs
@@ -1,10 +1,14 @@
 using Company.Business.Persistence.Database;
+using Empresa.Negocio.Application.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DatabaseService>(options=>
 options.UseSqlServer(builder.Configuration["SQLConnectionString"]) );
+
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
 app.Run();
