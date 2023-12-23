018c8a16c7294e8d66c978d292d3a921772ef649
IodenKisuka <iodenteken@hotmail.com>
2023-12-22 22:44:20 -0300

    modificate name field of userEntity

--- src/Api/Company.Business.Api/Program.cs
+++ src/Api/Company.Business.Api/Program.cs
@@ -1,14 +1,29 @@
 using Company.Business.Persistence.Database;
 using Empresa.Negocio.Application.Interface;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DatabaseService>(options=>
-options.UseSqlServer(builder.Configuration["SQLConnectionString"]) );
+builder.Services.AddDbContext<DatabaseService>(options =>
+options.UseSqlServer(builder.Configuration["SQLConnectionString"]));
 
 builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
+/*pruebas 
+ */
+app.MapPost("CreateTest", async (IDatabaseService _database) =>
+{
+    var entity = new Company.Business.Domain.Entities.User.UserEntity
+    {
+        FirstName = "Emiliano",
+        LastName = "Lopez",
+        UserName = "EmiLop",
+        Password = "Alumno1234?"
+    };
+    await _database.User.AddAsync(entity);
+    await _database.SaveAsync();
+    return "Create Ok";
+});
+
 app.Run();
--- src/Core/Company.Business.Domain/Entities/User/UserEntity.cs
+++ src/Core/Company.Business.Domain/Entities/User/UserEntity.cs
@@ -8,7 +8,7 @@ namespace Company.Business.Domain.Entities.User
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
-        public string PassWord { get; set; }
+        public string Password { get; set; }
         public ICollection<BookingEntity> Bookings { get; set; }
     }
 }
